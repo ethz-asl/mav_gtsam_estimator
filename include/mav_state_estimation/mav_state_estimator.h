299573afd6e5e9039d98b1adac38ddb9e977a76d
Rik Bähnemann <brik@ethz.ch>
2020-08-19 18:32:00 +0200

    First TRIAD initialization.

--- include/mav_state_estimation/mav_state_estimator.h
+++ include/mav_state_estimation/mav_state_estimator.h
@@ -1,12 +1,13 @@
 #ifndef MAV_STATE_ESTIMATOR_MAV_STATE_ESTIMATOR_H_
 #define MAV_STATE_ESTIMATOR_MAV_STATE_ESTIMATOR_H_
 
+#include <Eigen/Dense>
+#include <optional>
 #include <piksi_rtk_msgs/PositionWithCovarianceStamped.h>
 #include <piksi_rtk_msgs/VelocityWithCovarianceStamped.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
-#include <Eigen/Dense>
-#include <optional>
+#include <tf2_ros/transform_listener.h>
 
 namespace mav_state_estimation {
 
@@ -15,12 +16,20 @@ public:
   MavStateEstimator();
 
 private:
+  Eigen::Vector3d getVectorFromParams(const std::string &param) const;
+
   void imuCallback(const sensor_msgs::Imu::ConstPtr &imu_msg);
   void posCallback(
       const piksi_rtk_msgs::PositionWithCovarianceStamped::ConstPtr &pos_msg);
   void
   baselineCallback(const piksi_rtk_msgs::PositionWithCovarianceStamped::ConstPtr
                        &baseline_msg);
+
+  void calculateTriadOrientation(const Eigen::Vector3d &R1,
+                                 const Eigen::Vector3d &r1,
+                                 const Eigen::Vector3d &R2,
+                                 const Eigen::Vector3d &r2,
+                                 Eigen::Quaterniond *q) const;
 
   ros::NodeHandle nh_;
   ros::NodeHandle nh_private_;
@@ -29,8 +38,15 @@ private:
   ros::Subscriber pos_0_sub_;
   ros::Subscriber baseline_sub_;
 
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tfl = tf2_ros::TransformListener(tf_buffer_);
+
   // State initialization.
-  std::optional<Eigen::Vector3d> up_init_;
+  Eigen::Vector3d B_t_P_ = Eigen::Vector3d::Zero(); // Position receiver.
+  Eigen::Vector3d B_t_A_ = Eigen::Vector3d::Zero(); // Attitude receiver.
+  std::optional<Eigen::Vector3d> init_g_imu_;
+  std::optional<Eigen::Vector3d> heading_init_;
+  std::optional<Eigen::Vector3d> pos_init_;
 };
 
 } // namespace mav_state_estimation
--- src/mav_state_estimator.cc
+++ src/mav_state_estimator.cc
@@ -1,12 +1,18 @@
 #include "mav_state_estimation/mav_state_estimator.h"
 
 #include <eigen_conversions/eigen_msg.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <ros/ros.h>
 
 namespace mav_state_estimation {
 
 MavStateEstimator::MavStateEstimator()
     : nh_(ros::NodeHandle()), nh_private_(ros::NodeHandle("~")) {
+  // Get parameters.
+  B_t_P_ = getVectorFromParams("position_receiver/B_t");
+  ROS_INFO_STREAM("Initial guess B_t_P: " << B_t_P_.transpose());
+  B_t_A_ = getVectorFromParams("attitude_receiver/B_t");
+  ROS_INFO_STREAM("Initial guess B_t_A: " << B_t_A_.transpose());
 
   // Subscribe to topics.
   const uint32_t kQueueSize = 1000;
@@ -21,17 +27,29 @@ MavStateEstimator::MavStateEstimator()
   ROS_INFO("Subscribing to: %s", baseline_sub_.getTopic().c_str());
 }
 
+Eigen::Vector3d
+MavStateEstimator::getVectorFromParams(const std::string &param) const {
+  std::vector<double> vec;
+  nh_private_.getParam(param, vec);
+  Eigen::Vector3d eig = Eigen::Vector3d::Zero();
+  if (vec.size() == 3) {
+    eig = Eigen::Vector3d(vec.data());
+  } else {
+    ROS_WARN("Cannot process parameter: %s Vector size: %lu", param.c_str(),
+             vec.size());
+  }
+  return eig;
+}
+
 void MavStateEstimator::imuCallback(const sensor_msgs::Imu::ConstPtr &imu_msg) {
   ROS_INFO_ONCE("Received first IMU message.");
-  if (up_init_) {
-
-  } else {
+  if (!init_g_imu_) {
     // Initialize gravity direction with first measurement.
     // Assumes no motion, no bias.
-    up_init_ = std::make_optional<Eigen::Vector3d>();
-    tf::vectorMsgToEigen(imu_msg->linear_acceleration, *up_init_);
-    up_init_->normalize();
-    ROS_INFO_STREAM("Initialized up direction: " << up_init_->transpose());
+    init_g_imu_ = std::make_optional<Eigen::Vector3d>();
+    tf::vectorMsgToEigen(imu_msg->linear_acceleration, *init_g_imu_);
+    *init_g_imu_ *= -1.0;
+    ROS_INFO_STREAM("Initialized up direction: " << init_g_imu_->transpose());
   }
 }
 
@@ -44,6 +62,44 @@ void MavStateEstimator::baselineCallback(
     const piksi_rtk_msgs::PositionWithCovarianceStamped::ConstPtr
         &baseline_msg) {
   ROS_INFO_ONCE("Received first BASELINE message.");
+  if (!heading_init_ && init_g_imu_) {
+    Eigen::Vector3d R2;
+    tf::pointMsgToEigen(baseline_msg->position.position, R2);
+    Eigen::Vector3d r2 = B_t_A_ - B_t_P_;
+
+    // TRIAD method. Correspondences:
+    // 1. Gravitation in NED frame R1, and IMU frame r1
+    // 2. Moving baseline heading in NED frame R2, and IMU frame r2
+    calculateTriadOrientation(Eigen::Vector3d(0, 0, 1), *init_g_imu_, R2, r2,
+                              NULL);
+    heading_init_ = std::make_optional<Eigen::Vector3d>();
+  }
+}
+
+void MavStateEstimator::calculateTriadOrientation(const Eigen::Vector3d &R1,
+                                                  const Eigen::Vector3d &r1,
+                                                  const Eigen::Vector3d &R2,
+                                                  const Eigen::Vector3d &r2,
+                                                  Eigen::Quaterniond *q) const {
+  ROS_ASSERT(q);
+
+  // Calculate the rotation between coordinate frames given two linearly
+  // independent measurements. https://en.wikipedia.org/wiki/Triad_method
+  Eigen::Vector3d S = R1.normalized();
+  Eigen::Vector3d s = r1.normalized();
+  Eigen::Vector3d M = (R1.cross(R2)).normalized();
+  Eigen::Vector3d m = (r1.cross(r2)).normalized();
+
+  Eigen::Matrix3d a;
+  a << s, m, s.cross(m);
+  ROS_INFO_STREAM("\n" << a);
+  Eigen::Matrix3d b;
+  b << S, M, S.cross(M);
+  ROS_INFO_STREAM("\n" << b);
+
+  Eigen::Matrix3d A = b * a.transpose();
+
+  ROS_INFO_STREAM("\n" << A);
 }
 
 } // namespace mav_state_estimation

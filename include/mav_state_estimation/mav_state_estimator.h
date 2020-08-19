2ba57171958c3eecbda2839301fa8985b963c58a
Rik Bähnemann <brik@ethz.ch>
2020-08-19 13:29:21 +0200

    Initialize UP direction.

--- include/mav_state_estimation/mav_state_estimator.h
+++ include/mav_state_estimation/mav_state_estimator.h
@@ -5,6 +5,8 @@
 #include <piksi_rtk_msgs/VelocityWithCovarianceStamped.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
+#include <Eigen/Dense>
+#include <optional>
 
 namespace mav_state_estimation {
 
@@ -16,14 +18,21 @@ private:
   void imuCallback(const sensor_msgs::Imu::ConstPtr &imu_msg);
   void posCallback(
       const piksi_rtk_msgs::PositionWithCovarianceStamped::ConstPtr &pos_msg);
+  void
+  baselineCallback(const piksi_rtk_msgs::PositionWithCovarianceStamped::ConstPtr
+                       &baseline_msg);
 
   ros::NodeHandle nh_;
   ros::NodeHandle nh_private_;
 
   ros::Subscriber imu_sub_;
   ros::Subscriber pos_0_sub_;
+  ros::Subscriber baseline_sub_;
+
+  // State initialization.
+  std::optional<Eigen::Vector3d> up_init_;
 };
 
-} // namespace mav_state_estimator
+} // namespace mav_state_estimation
 
 #endif // MAV_STATE_ESTIMATOR_MAV_STATE_ESTIMATOR_H_
--- src/mav_state_estimator.cc
+++ src/mav_state_estimator.cc
@@ -1,5 +1,6 @@
 #include "mav_state_estimation/mav_state_estimator.h"
 
+#include <eigen_conversions/eigen_msg.h>
 #include <ros/ros.h>
 
 namespace mav_state_estimation {
@@ -10,20 +11,39 @@ MavStateEstimator::MavStateEstimator()
   // Subscribe to topics.
   const uint32_t kQueueSize = 1000;
   imu_sub_ =
-      nh_.subscribe("imu", kQueueSize, &MavStateEstimator::imuCallback, this);
+      nh_.subscribe("imu0", kQueueSize, &MavStateEstimator::imuCallback, this);
   ROS_INFO("Subscribing to: %s", imu_sub_.getTopic().c_str());
   pos_0_sub_ =
       nh_.subscribe("pos0", kQueueSize, &MavStateEstimator::posCallback, this);
   ROS_INFO("Subscribing to: %s", pos_0_sub_.getTopic().c_str());
+  baseline_sub_ = nh_.subscribe("baseline0", kQueueSize,
+                                &MavStateEstimator::baselineCallback, this);
+  ROS_INFO("Subscribing to: %s", baseline_sub_.getTopic().c_str());
 }
 
 void MavStateEstimator::imuCallback(const sensor_msgs::Imu::ConstPtr &imu_msg) {
   ROS_INFO_ONCE("Received first IMU message.");
+  if (up_init_) {
+
+  } else {
+    // Initialize gravity direction with first measurement.
+    // Assumes no motion, no bias.
+    up_init_ = std::make_optional<Eigen::Vector3d>();
+    tf::vectorMsgToEigen(imu_msg->linear_acceleration, *up_init_);
+    up_init_->normalize();
+    ROS_INFO_STREAM("Initialized up direction: " << up_init_->transpose());
+  }
 }
 
 void MavStateEstimator::posCallback(
     const piksi_rtk_msgs::PositionWithCovarianceStamped::ConstPtr &pos_msg) {
   ROS_INFO_ONCE("Received first POS message.");
+}
+
+void MavStateEstimator::baselineCallback(
+    const piksi_rtk_msgs::PositionWithCovarianceStamped::ConstPtr
+        &baseline_msg) {
+  ROS_INFO_ONCE("Received first BASELINE message.");
 }
 
 } // namespace mav_state_estimation
